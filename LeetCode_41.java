00b0ad1c4d7fc76569a5ac175ed885928270b9dc
banusrisathyan <168272354+banusrisathyan@users.noreply.github.com>
2025-04-12 16:40:29 +0530

    Create LeetCode_41.java

---
+++ LeetCode_41.java
@@ -0,0 +1,60 @@
+
+Code
+Testcase
+Test Result
+Test Result
+41. First Missing Positive
+Solved
+Hard
+Topics
+Companies
+Hint
+Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+Example 3:
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+Explanation: The smallest positive integer 1 is missing.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+
+SOLUTION:
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int t =0;
+        for(int i=1;i<nums.length;i++){
+            while(nums[i]>0&&nums[i]<nums.length&&nums[nums[i]-1]!=nums[i]){
+                t =nums[nums[i]-1];
+                nums[nums[i]-1] = nums[i];
+                nums[i] =t;
+            }
+        }
+        //for(int num:nums)System.out.println(num);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i+1) 
+            return i+1;
+            
+        }
+        return nums.length+1;
+    }
+}
